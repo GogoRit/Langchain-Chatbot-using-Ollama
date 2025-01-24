f6aaeaa415891d9404957a013f689669a315cbcd
gogo2710 <gm8189@rit.edu>
2025-01-24 14:29:43 -0500

    Project files from local directory

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,39 +1,33 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-
 from langchain_community.llms import Ollama
-
-import os
 import streamlit as st
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
-#Prompt Template
+## Prompt Template
 
-prompt = ChatPromptTemplate.from_messages(
+prompt=ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a helpful assistant. Please respond to the following user inquiries."),
-        ("user", "Question: {question}"),
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
     ]
 )
+## streamlit framework
 
-# Streamlit App
+st.title('Langchain Demo')
+input_text=st.text_input("Search the topic u want")
 
-st.title("Langchain Chatbot")
-input_text = st.text_input("Enter your question here:")
-
-# OLLAMA Chatbot
-
-ollama = Ollama(model = "llama2")
-output_parser = StrOutputParser()
-chain = prompt | ollama | output_parser
+# ollama LLAma2 LLm 
+llm=Ollama(model="llama2")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
 
 if input_text:
-    st.write(chain.invoke({"question": input_text}))
-
+    st.write(chain.invoke({"question":input_text}))
